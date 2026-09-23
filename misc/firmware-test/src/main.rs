aa864805d88a76f257ce9d024e280a80e3792617
Antoine Jaussoin <antoinejaussoin@users.noreply.github.com>
2026-09-23 20:40:18 +0100

    Test Firmware (#18)
    * test firmware
    
    * again

---
+++ misc/firmware-test/src/main.rs
@@ -0,0 +1,79 @@
+//! Blink the Pico LiPo 2 XL W user LED (RM2 `WL_GPIO0`) every second.
+
+#![no_std]
+#![no_main]
+
+use cyw43_pio::{PioSpi, RM2_CLOCK_DIVIDER};
+use embassy_executor::Spawner;
+use embassy_rp::bind_interrupts;
+use embassy_rp::block::ImageDef;
+use embassy_rp::dma;
+use embassy_rp::gpio::{Level, Output};
+use embassy_rp::peripherals::{DMA_CH0, PIO0};
+use embassy_rp::pio::{InterruptHandler as PioInterruptHandler, Pio};
+use embassy_time::Timer;
+use panic_halt as _;
+use static_cell::StaticCell;
+
+bind_interrupts!(struct Irqs {
+    PIO0_IRQ_0 => PioInterruptHandler<PIO0>;
+    DMA_IRQ_0 => dma::InterruptHandler<DMA_CH0>;
+});
+
+#[unsafe(link_section = ".start_block")]
+#[used]
+static IMAGE_DEF: ImageDef = ImageDef::secure_exe();
+
+#[unsafe(link_section = ".bi_entries")]
+#[used]
+static PICOTOOL_ENTRIES: [embassy_rp::binary_info::EntryAddr; 3] = [
+    embassy_rp::binary_info::rp_program_name!(c"firmware-test"),
+    embassy_rp::binary_info::rp_cargo_version!(),
+    embassy_rp::binary_info::rp_program_description!(c"Blink the user LED every second"),
+];
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+
+    let fw = cyw43::aligned_bytes!("../../../firmware/cyw43-firmware/43439A0.bin");
+    let clm = cyw43::aligned_bytes!("../../../firmware/cyw43-firmware/43439A0_clm.bin");
+    let nvram = cyw43::aligned_bytes!("../../../firmware/cyw43-firmware/nvram_rp2040.bin");
+
+    let pwr = Output::new(p.PIN_23, Level::Low);
+    let cs = Output::new(p.PIN_25, Level::High);
+    let mut pio = Pio::new(p.PIO0, Irqs);
+    let spi = PioSpi::new(
+        &mut pio.common,
+        pio.sm0,
+        RM2_CLOCK_DIVIDER,
+        pio.irq0,
+        cs,
+        p.PIN_24,
+        p.PIN_29,
+        dma::Channel::new(p.DMA_CH0, Irqs),
+    );
+
+    static STATE: StaticCell<cyw43::State> = StaticCell::new();
+    let (_net, mut control, runner) =
+        cyw43::new(STATE.init(cyw43::State::new()), pwr, spi, fw, nvram).await;
+    spawner.spawn(cyw43_task(runner).unwrap());
+    control.init(clm.as_ref()).await;
+    control
+        .set_power_management(cyw43::PowerManagementMode::None)
+        .await;
+
+    loop {
+        control.gpio_set(0, true).await;
+        Timer::after_secs(1).await;
+        control.gpio_set(0, false).await;
+        Timer::after_secs(1).await;
+    }
+}
+
+#[embassy_executor::task]
+async fn cyw43_task(
+    runner: cyw43::Runner<'static, cyw43::SpiBus<Output<'static>, PioSpi<'static, PIO0, 0>>>,
+) -> ! {
+    runner.run().await
+}
